982a59414e991f7f4c5f53b436bc01cc1adb625b
Thomas Boerger <thomas@webhippie.de>
2022-07-19 23:36:03 +0200

    chore: add modern cmd implementation

---
+++ cmd/jenkins_exporter/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/promhippie/jenkins_exporter/pkg/command"
+)
+
+func main() {
+	if env := os.Getenv("JENKINS_EXPORTER_ENV_FILE"); env != "" {
+		godotenv.Load(env)
+	}
+
+	if err := command.Run(); err != nil {
+		os.Exit(1)
+	}
+}
