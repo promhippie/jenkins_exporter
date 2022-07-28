promhippie/scw_exporter

chore: update hack scripts

hack/generate-envvars-docs.go
@@ -1,0 +1,118 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/promhippie/scw_exporter/pkg/command"
+	"github.com/promhippie/scw_exporter/pkg/config"
+	"github.com/urfave/cli/v2"
+)
+
+type flag struct {
+	Flag    string
+	Default string
+	Envs    []string
+	Help    string
+	List    bool
+}
+
+func main() {
+	flags := make([]flag, 0)
+
+	for _, f := range command.RootFlags(config.Load()) {
+		switch v := f.(type) {
+		case *cli.StringFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: v.Value,
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    false,
+			})
+		case *cli.IntFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: strconv.Itoa(v.Value),
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    false,
+			})
+		case *cli.BoolFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: fmt.Sprintf("%+v", v.Value),
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    false,
+			})
+		case *cli.DurationFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: v.Value.String(),
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    false,
+			})
+		case *cli.StringSliceFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: strings.Join(v.Value.Value(), ", "),
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    true,
+			})
+		default:
+			fmt.Printf("unknown type: %s\n", v)
+			os.Exit(1)
+		}
+	}
+
+	f, err := os.Create("docs/partials/envvars.md")
+
+	if err != nil {
+		fmt.Printf("failed to create file")
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	last := flags[len(flags)-1]
+	for _, row := range flags {
+		f.WriteString(
+			strings.Join(
+				row.Envs,
+				", ",
+			) + "\n",
+		)
+
+		f.WriteString(fmt.Sprintf(
+			": %s",
+			row.Help,
+		))
+
+		if row.List {
+			f.WriteString(
+				", comma-separated list",
+			)
+		}
+
+		if row.Default != "" {
+			f.WriteString(fmt.Sprintf(
+				", defaults to `%s`",
+				row.Default,
+			))
+		}
+
+		f.WriteString("\n")
+
+		if row.Flag != last.Flag {
+			f.WriteString("\n")
+		}
+	}
+}

hack/generate-metrics-docs.go
@@ -1,0 +1,115 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"sort"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/promhippie/scw_exporter/pkg/config"
+	"github.com/promhippie/scw_exporter/pkg/exporter"
+)
+
+type metric struct {
+	Name   string
+	Help   string
+	Labels []string
+}
+
+func main() {
+	collectors := make([]*prometheus.Desc, 0)
+
+	collectors = append(
+		collectors,
+		exporter.NewDashboardCollector(nil, nil, nil, nil, config.Load().Target).Metrics()...,
+	)
+
+	collectors = append(
+		collectors,
+		exporter.NewSecurityGroupCollector(nil, nil, nil, nil, config.Load().Target).Metrics()...,
+	)
+
+	collectors = append(
+		collectors,
+		exporter.NewServerCollector(nil, nil, nil, nil, config.Load().Target).Metrics()...,
+	)
+
+	collectors = append(
+		collectors,
+		exporter.NewSnapshotCollector(nil, nil, nil, nil, config.Load().Target).Metrics()...,
+	)
+
+	collectors = append(
+		collectors,
+		exporter.NewVolumeCollector(nil, nil, nil, nil, config.Load().Target).Metrics()...,
+	)
+
+	metrics := make([]metric, 0)
+
+	metrics = append(metrics, metric{
+		Name:   "scw_request_duration_seconds",
+		Help:   "Histogram of latencies for requests to the api per collector",
+		Labels: []string{"collector"},
+	})
+
+	metrics = append(metrics, metric{
+		Name:   "scw_request_failures_total",
+		Help:   "Total number of failed requests to the api per collector",
+		Labels: []string{"collector"},
+	})
+
+	for _, desc := range collectors {
+		m := metric{
+			Name:   reflect.ValueOf(desc).Elem().FieldByName("fqName").String(),
+			Help:   reflect.ValueOf(desc).Elem().FieldByName("help").String(),
+			Labels: make([]string, 0),
+		}
+
+		labels := reflect.ValueOf(desc).Elem().FieldByName("variableLabels")
+
+		for i := 0; i < labels.Len(); i++ {
+			m.Labels = append(m.Labels, labels.Index(i).String())
+		}
+
+		metrics = append(metrics, m)
+	}
+
+	sort.SliceStable(metrics, func(i, j int) bool {
+		return metrics[i].Name < metrics[j].Name
+	})
+
+	f, err := os.Create("docs/partials/metrics.md")
+
+	if err != nil {
+		fmt.Printf("failed to create file")
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	last := metrics[len(metrics)-1]
+	for _, m := range metrics {
+		f.WriteString(fmt.Sprintf(
+			"%s{%s}\n",
+			m.Name,
+			strings.Join(
+				m.Labels,
+				", ",
+			),
+		))
+
+		f.WriteString(fmt.Sprintf(
+			": %s\n",
+			m.Help,
+		))
+
+		if m.Name != last.Name {
+			f.WriteString("\n")
+		}
+	}
+}

promhippie/jenkins_exporter

hack/generate-envvars-docs.go
@@ -1,0 +1,118 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/promhippie/jenkins_exporter/pkg/command"
+	"github.com/promhippie/jenkins_exporter/pkg/config"
+	"github.com/urfave/cli/v2"
+)
+
+type flag struct {
+	Flag    string
+	Default string
+	Envs    []string
+	Help    string
+	List    bool
+}
+
+func main() {
+	flags := make([]flag, 0)
+
+	for _, f := range command.RootFlags(config.Load()) {
+		switch v := f.(type) {
+		case *cli.StringFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: v.Value,
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    false,
+			})
+		case *cli.IntFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: strconv.Itoa(v.Value),
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    false,
+			})
+		case *cli.BoolFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: fmt.Sprintf("%+v", v.Value),
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    false,
+			})
+		case *cli.DurationFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: v.Value.String(),
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    false,
+			})
+		case *cli.StringSliceFlag:
+			flags = append(flags, flag{
+				Flag:    v.Name,
+				Default: strings.Join(v.Value.Value(), ", "),
+				Envs:    v.EnvVars,
+				Help:    v.Usage,
+				List:    true,
+			})
+		default:
+			fmt.Printf("unknown type: %s\n", v)
+			os.Exit(1)
+		}
+	}
+
+	f, err := os.Create("docs/partials/envvars.md")
+
+	if err != nil {
+		fmt.Printf("failed to create file")
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	last := flags[len(flags)-1]
+	for _, row := range flags {
+		f.WriteString(
+			strings.Join(
+				row.Envs,
+				", ",
+			) + "\n",
+		)
+
+		f.WriteString(fmt.Sprintf(
+			": %s",
+			row.Help,
+		))
+
+		if row.List {
+			f.WriteString(
+				", comma-separated list",
+			)
+		}
+
+		if row.Default != "" {
+			f.WriteString(fmt.Sprintf(
+				", defaults to `%s`",
+				row.Default,
+			))
+		}
+
+		f.WriteString("\n")
+
+		if row.Flag != last.Flag {
+			f.WriteString("\n")
+		}
+	}
+}

hack/generate-metrics-docs.go
@@ -1,0 +1,95 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"sort"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/promhippie/jenkins_exporter/pkg/config"
+	"github.com/promhippie/jenkins_exporter/pkg/exporter"
+)
+
+type metric struct {
+	Name   string
+	Help   string
+	Labels []string
+}
+
+func main() {
+	collectors := make([]*prometheus.Desc, 0)
+
+	collectors = append(
+		collectors,
+		exporter.NewJobCollector(nil, nil, nil, nil, config.Load().Target).Metrics()...,
+	)
+
+	metrics := make([]metric, 0)
+
+	metrics = append(metrics, metric{
+		Name:   "jenkins_request_duration_seconds",
+		Help:   "Histogram of latencies for requests to the api per collector",
+		Labels: []string{"collector"},
+	})
+
+	metrics = append(metrics, metric{
+		Name:   "jenkins_request_failures_total",
+		Help:   "Total number of failed requests to the api per collector",
+		Labels: []string{"collector"},
+	})
+
+	for _, desc := range collectors {
+		m := metric{
+			Name:   reflect.ValueOf(desc).Elem().FieldByName("fqName").String(),
+			Help:   reflect.ValueOf(desc).Elem().FieldByName("help").String(),
+			Labels: make([]string, 0),
+		}
+
+		labels := reflect.ValueOf(desc).Elem().FieldByName("variableLabels")
+
+		for i := 0; i < labels.Len(); i++ {
+			m.Labels = append(m.Labels, labels.Index(i).String())
+		}
+
+		metrics = append(metrics, m)
+	}
+
+	sort.SliceStable(metrics, func(i, j int) bool {
+		return metrics[i].Name < metrics[j].Name
+	})
+
+	f, err := os.Create("docs/partials/metrics.md")
+
+	if err != nil {
+		fmt.Printf("failed to create file")
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	last := metrics[len(metrics)-1]
+	for _, m := range metrics {
+		f.WriteString(fmt.Sprintf(
+			"%s{%s}\n",
+			m.Name,
+			strings.Join(
+				m.Labels,
+				", ",
+			),
+		))
+
+		f.WriteString(fmt.Sprintf(
+			": %s\n",
+			m.Help,
+		))
+
+		if m.Name != last.Name {
+			f.WriteString("\n")
+		}
+	}
+}